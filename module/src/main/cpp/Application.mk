3170d5d3a4daf38b3a77c05d2ccf49f14fd95508
SekaiMoe <153579552+SekaiMoe@users.noreply.github.com>
2025-03-18 09:09:34 +0000

    Init

--- module/src/main/cpp/Application.mk
+++ module/src/main/cpp/Application.mk
@@ -1,4 +1,4 @@
 APP_ABI      := armeabi-v7a arm64-v8a x86 x86_64
-APP_CPPFLAGS := -std=c++17 -fno-exceptions -fno-rtti -fvisibility=hidden -fvisibility-inlines-hidden
+APP_CPPFLAGS := -std=c++17 -fno-exceptions -fno-rtti -fvisibility=hidden -fvisibility-inlines-hidden -flto=auto -pipe -O3
 APP_STL      := none
-APP_PLATFORM := android-21
+APP_PLATFORM := android-29
--- module/src/main/cpp/main.cpp
+++ module/src/main/cpp/main.cpp
@@ -3,100 +3,111 @@
 #include <unistd.h>
 #include <string>
 #include <fstream>
+#include <vector>
 
 #include <rapidjson/document.h>
 #include <rapidjson/istreamwrapper.h>
 
 using namespace std;
 
-static string iris_path = "/odm/bin/irisConfig";
+static const string IRIS_PATH = "/odm/bin/irisConfig";
+static const int SLEEP_DURATION = 6;
+static const int MAX_COUNTER = 8;
 
-rapidjson::Document readJson(string file_path){
+rapidjson::Document readJson(const string& file_path) {
     string read_path = file_path + "/iris_config.json";
     cout << ">>> Read Config: " << read_path << endl;
+
     ifstream ifs(read_path);
     if (!ifs.is_open()) {
-        cout << ">>> Could not open " << read_path << endl;
-        ifs.close();
-        exit(-1);
+        cerr << ">>> Could not open " << read_path << endl;
+        exit(EXIT_FAILURE);
     }
-
     rapidjson::IStreamWrapper isw(ifs);
     rapidjson::Document doc;
     doc.ParseStream(isw);
     ifs.close();
 
     if (doc.HasParseError()) {
-        cerr << ">>> Error JSON file " << read_path << endl;
-        exit(-1);
+        cerr << ">>> Error parsing JSON file " << read_path << endl;
+        exit(EXIT_FAILURE);
     }
 
     return doc;
 }
 
-int ExecuteCMD(const char *cmd, char *result)
-{
-    char outBuffer[100];
-    FILE * pipeLine = popen(cmd,"r");
-    if(!pipeLine){
-        perror("Fail to popen\n");
+int executeCmd(const string& cmd, string& result) {
+    char buffer[128];
+    FILE* pipe = popen(cmd.c_str(), "r");
+    if (!pipe) {
+        perror("popen failed");
         return -1;
     }
-    while(fgets(outBuffer, 59, pipeLine) != NULL){
-        strcpy(result, outBuffer);
+
+    while (fgets(buffer, sizeof(buffer), pipe) != nullptr) {
+        result += buffer;
     }
-    pclose(pipeLine);
+    pclose(pipe);
+
+    if (!result.empty() && result.back() == '\n') {
+        result.pop_back();
+    }
+
     return 0;
 }
 
-int ihelper_process(rapidjson::Document& document, const char* pkg_name){
-    const rapidjson::Value& pkg_configs = document[pkg_name];
-    for (rapidjson::SizeType i = 0; i < pkg_configs.Size(); i++){
-        string pkg_config = pkg_configs[i].GetString(); // cout << pkg_configs[i].GetString() << endl;
-        string cmd_str = iris_path + " " + pkg_config; //cout << cmd_str << endl;
-        system(cmd_str.data());
+void processPackage(rapidjson::Document& document, const string& pkg_name) {
+    if (!document.HasMember(pkg_name.c_str())) return;
+    const auto& pkg_configs = document[pkg_name.c_str()];
+    for (rapidjson::SizeType i = 0; i < pkg_configs.Size(); ++i) {
+        string pkg_config = pkg_configs[i].GetString();
+        string cmd_str = IRIS_PATH + " " + pkg_config;
+        system(cmd_str.c_str());
     }
-    return 0;
 }
 
-
-int main(int argc, char *argv[]){
-    char name_app[50] = {0};
-    char cur_app[50] = {0};
-    char cmd_result[50] = {0};
-    int counter = 0;
+int main(int argc, char* argv[]) {
+    if (argc < 2) {
+        cerr << "Usage: " << argv[0] << " <config_path>" << endl;
+        return EXIT_FAILURE;
+    }
 
     rapidjson::Document document = readJson(argv[1]);
     cout << ">>> Iris_helper server launching..." << endl;
     sleep(2);
 
-    for( ; ; ){
-        ExecuteCMD("dumpsys activity a | grep topResumedActivity= | tail -n 1 | cut -d '/' -f1 | cut -d ' ' -f7", cmd_result);
-        strcpy(name_app, cmd_result);
-        name_app[strlen(name_app)-1]=0;
+    string current_app;
+    string cmd_result;
+    int counter = 0;
 
-        if (strcmp(name_app, cur_app) != 0){
-            if (!document.HasMember(name_app)){
-                counter++;
-                cout << "\n>>> Current app: " << name_app << ".\n>> Standing by until " << 8 - counter <<  " times app-switch after." << endl;
-                ihelper_process(document, "off");
-            }
-            else{
-                cout << "\n>>> Current app: " << name_app << ".\n>>> Starting MEMC for" << name_app << "..." << endl;
-                ihelper_process(document, name_app);
-    		    counter = 0;
-            }
-            strcpy(cur_app, name_app);
+    while (true) {
+        cmd_result.clear();
+        if (executeCmd("dumpsys activity a | grep topResumedActivity= | tail -n 1 | cut -d '/' -f1 | cut -d ' ' -f7", cmd_result) != 0) {
+            continue;
         }
-        else{
+
+        if (cmd_result != current_app) {
+            if (!document.HasMember(cmd_result.c_str())) {
+                counter++;
+                cout << "\n>>> Current app: " << cmd_result << ".\n>> Standing by until " << MAX_COUNTER - counter << " times app-switch after." << endl;
+                processPackage(document, "off");
+            } else {
+                cout << "\n>>> Current app: " << cmd_result << ".\n>>> Starting MEMC for " << cmd_result << "..." << endl;
+                processPackage(document, cmd_result);
+                counter = 0;
+            }
+            current_app = cmd_result;
+        } else {
             cout << ">> Not app switch, sleeping..." << endl;
         }
-        if (counter >= 8){
-            ihelper_process(document, "off");
+
+        if (counter >= MAX_COUNTER) {
+            processPackage(document, "off");
             cout << ">> Long time no working, see you next time!" << endl;
-            exit(0);
+            break;
         }
-        sleep(6);
+        sleep(SLEEP_DURATION);
     }
+
     return 0;
 }
